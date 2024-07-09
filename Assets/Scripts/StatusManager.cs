84ff2e60a2b0fc980605651d4d028c581564ba4c
shahan-kimura <suteado01092@gmail.com>
2024-07-10 07:19:16 +0900

    StatusManager.csを追加 Enemyに付加してHPとダメージ判定が可能に 衝突判定のため、EnemyにはRigidbodyを付加すること

---
+++ Assets/Scripts/StatusManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatusManager : MonoBehaviour
+{
+    [SerializeField] GameObject MainObject; //このスクリプトをアタッチするオブジェクト
+    [SerializeField] int HP =1;             //HP現在値
+    [SerializeField] int MaxHP =1;          //いずれMaxHP利用する際に使用
+
+    [SerializeField] GameObject destroyEffect;  //撃破エフェクト
+
+    [SerializeField] string TagName;            //当たり判定となるタグ
+
+    // Update is called once per frame
+    void Update()
+    {
+        //HPが0以下なら、撃破エフェクトを生成してMainを破壊
+        if (HP <= 0)
+        {
+            HP = 0;
+            var effect = Instantiate(destroyEffect);
+            effect.transform.position = transform.position;
+            Destroy(effect,5);
+            Destroy(MainObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Hit");
+        //当たり判定のタグネームと一致していればダメージ処理呼び出し
+        if (other.CompareTag(TagName))
+        {
+            Damage();
+        }
+    }
+    private void Damage()
+    {
+        HP--;
+    }
+
+}
